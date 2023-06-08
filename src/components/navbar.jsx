a191a756b843a91f8740260fb55e5f870af652ab
Kachi <125273593+KachiBlockchain@users.noreply.github.com>
2023-06-08 16:17:52 +0100

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
     <div className="navbar">
       <div className="links">
         <Link to="/"> Shop </Link>
-        <Link to="/contact"> Contact </Link>
+        <a href="https://kachitech.com.ng"> Contact </a>
         <Link to="/cart">
           <ShoppingCart size={32} />
         </Link>
